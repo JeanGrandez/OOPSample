54685990828134e2f51fa9af50e5cf37e1435f22
JeanGrandez <jpmle10@hotmail.com>
2024-08-22 16:40:31 -0500

    feat(sales-order): added sales order item class.

---
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrderItem.cs
@@ -0,0 +1,33 @@
+namespace OOPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrderItem
+{
+    public SalesOrderItem(Guid salesOrderId, int productId, int quantity, double unitPrice)
+    {
+        if(salesOrderId==Guid.Empty)
+            throw new ArgumentException("Sales Order Id is required");
+        if(productId<=0)
+            throw new ArgumentException("Product ID is required");
+        if(quantity<=0)
+            throw new ArgumentException("Quantity must be greater than 0");
+        if(unitPrice<=0)
+            throw new ArgumentException("Unit Price must be greater than 0");
+        SalesOrderId = salesOrderId;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
+
+    public double CalculateItemPrice() => Quantity * UnitPrice;
+    
+    public Guid Id { get; private set; } = GenerateOrderItemId(); //UUID Java
+
+    public Guid SalesOrderId { get;}
+    public int ProductId { get; }
+    public int Quantity { get; }
+    public double UnitPrice { get; }
+    private static Guid GenerateOrderItemId()
+    {
+        return Guid.NewGuid();
+    }
+}
